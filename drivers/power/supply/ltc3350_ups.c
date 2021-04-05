173ba2c3caa4429366b6e0d3c64d5deb63d96c2e
inzenjer-banderas <lazar.neg@hotmail.com>
2021-04-05 09:53:08 +0200

    Switched to regular static funcs and other modifications

--- drivers/power/supply/ltc3350_ups.c
+++ drivers/power/supply/ltc3350_ups.c
@@ -8,27 +8,37 @@
 
 struct ltc3350 {
 	struct i2c_client *client;
-	struct device *dev;
-	struct client_ops *ltc3350_client_ops;
+	//struct device *dev;
+	//struct client_ops *ltc3350_client_ops;
 };
+//
+//struct client_ops {
+//	int (*read_value)(const struct i2c_client *client, u8 reg);
+//	int (*write_value)(const struct i2c_client *client, u8 reg, u16 value);
+//};
 
-struct client_ops {
-	int (*read_value)(struct i2c_client *client, u8 reg);
-	int (*write_value)(struct i2c_client *client, u8 reg, u16 value);
-};
-
-const void *ltc3350_data;
+//static const void *ltc3350_data;
 //static unsigned long ltc3350_private_data;
 
-static int ltc3350_read_value(struct i2c_client *client, u8 reg)
+static s32 ltc3350_read_value(const struct i2c_client *client, u8 reg)
 {
 	return i2c_smbus_read_byte_data(client, reg);
 }
 
-static int ltc3350_write_value(struct i2c_client *client, u8 reg, u16 value)
+static s32 ltc3350_read_word_value(const struct i2c_client *client, u8 reg)
 {
-	return i2c_smbus_write_byte_data(client, reg, value);
+	return i2c_smbus_read_word_data(client, reg);
 }
+
+static s32 ltc3350_read_block_value(const struct i2c_client *client, u8 reg, u8 len, u8 *values)
+{
+	return i2c_smbus_read_i2c_block_data(client, reg, len, values);
+}
+
+//static int ltc3350_write_value(const struct i2c_client *client, u8 reg, u16 value)
+//{
+//	return i2c_smbus_write_byte_data(client, reg, value);
+//}
 
 static int ltc3350_probe(struct i2c_client *client,
 						 const struct i2c_device_id *id) {
@@ -36,6 +46,11 @@ static int ltc3350_probe(struct i2c_client *client,
 	u8 reg;
 	//u16 value__to_write;
 	u8 read_value;
+	u16 word_value;
+	s16 converted_data;
+	u8 block_values[2];
+//	float temp;
+	int i;
 	int ret = 0;
 
 	ltc3350 = devm_kzalloc(&client->dev, sizeof(struct ltc3350), GFP_KERNEL);
@@ -46,25 +61,60 @@ static int ltc3350_probe(struct i2c_client *client,
 	i2c_set_clientdata(client, ltc3350);
 
 	ltc3350->client = client;
-	ltc3350->ltc3350_client_ops->read_value = ltc3350_read_value;
-	ltc3350->ltc3350_client_ops->write_value = ltc3350_write_value;
+//	ltc3350->ltc3350_client_ops->read_value = ltc3350_read_value;
+//	ltc3350->ltc3350_client_ops->write_value = ltc3350_write_value;
 
 	reg = 0x2A;
-	read_value = ltc3350->ltc3350_client_ops->read_value(ltc3350->client, reg);
+	read_value = ltc3350_read_value(ltc3350->client, reg);
 	if (read_value){
-		pr_info("ltc3350: Read value for temp: %X", read_value);
+		pr_info("ltc3350: Read value for temp: 0x%X\n", read_value);
 	}
 	else {
-		pr_info("ltc3350: Unable to read value for temp!!");
+		pr_info("ltc3350: Unable to read value for temp!!\n");
+		ret = -1;
+	}
+	read_value = ltc3350_read_value(ltc3350->client, reg);
+	if (read_value){
+		pr_info("ltc3350: Read value for temp: 0x%d\n", read_value);
+	}
+	else {
+		pr_info("ltc3350: Unable to read value for temp!!\n");
 		ret = -1;
 	}
 
-	pr_info("ltc3350: Inside ltc3350 probe function");
+	word_value = ltc3350_read_word_value(ltc3350->client, reg);
+	if (read_value){
+		pr_info("ltc3350: Read value for temp: 0x%X\n", word_value);
+	}
+	else {
+		pr_info("ltc3350: Unable to read value for temp!!\n");
+		ret = -1;
+	}
+
+	ret = ltc3350_read_block_value(ltc3350->client, 0x2a, sizeof(block_values), block_values);
+	pr_info("ltc3350: ret block_value: %d\n", ret);
+	if (ret == sizeof(block_values)){
+		for (i=0; i<2; i++) {
+			pr_info("ltc3350: Read block_value[%d]: %d\n", i, block_values[i]);
+		}
+
+	}
+	else {
+		pr_info("ltc3350: Unable to read block value for temp!!\n");
+		ret = -1;
+	}
+
+	converted_data = (word_value & (0xff)) + (word_value & (0xff << 8)) * 256;
+	pr_info("ltc3350: Converted value for temp: %X\n", converted_data);
+//	temp = 0.028 * converted_data - 251.4;
+//	pr_info("ltc3350: Actual temp: %f\n", temp);
+
+	pr_info("ltc3350: Inside ltc3350 probe function\n");
 	return ret;
 };
 
 static int ltc3350_remove(struct i2c_client *client) {
-	pr_info("ltc3350: Inside ltc3350 remove function");
+	pr_info("ltc3350: Inside ltc3350 remove function\n");
 	return 0;
 };
 
